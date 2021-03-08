107900c5b160c542060f47c8b1ba6598e4d150e3
Karthik Dechiraju <karthik.dechiraju@plivo.com>
2021-03-08 15:04:25 +0530

    fix conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
-const fs = require("fs");
-const path = require("path");
-const webpack = require("webpack");
-const TerserPlugin = require("terser-webpack-plugin");
-const CreateFileWebpack = require('create-file-webpack')
-const package = require("./package");
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const package = require('./package');
 const version = package.version;
 
-module.exports = (env) => {
+module.exports = env => {
   const plugins = [
-    new webpack.BannerPlugin(fs.readFileSync("./LICENSE", "utf8")),
+    new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf8')),
     new webpack.EnvironmentPlugin({
-      PLIVO_ENV: env.PLIVO_ENV,
-    }),
+      PLIVO_ENV: env.PLIVO_ENV,
+    }),
   ];
-  if (env.production) {
-    plugins.push(new DtsBundlePlugin());
+  if (env.production){
+    plugins.push(new DtsBundlePlugin())
     plugins.push(function () {
       this.hooks.afterEmit.tap("IncludeHashPlugin", (stats) => {
         var replaceInFile = function (filePath, toReplace, replacement) {
@@ -39,7 +38,7 @@ module.exports = (env) => {
     });
 
     plugins.push(function () {
-      this.hooks.afterEmit.tap("IncludeHashPlugin", (stats) => {
+      this.hooks.afterEmit.tap("CreateHashPlugin", (stats) => {
         fs.appendFile(
           path.join(
             path.join(path.resolve(__dirname, "dist"), "hash.json")
@@ -52,32 +51,27 @@ module.exports = (env) => {
   }
   if (env.development) plugins.push(new webpack.NamedModulesPlugin());
   return {
-    mode:
-      env.development || env.nominify || env.npm ? "development" : "production",
-    entry: env.npm
-      ? ["./lib/index.ts"]
-      : ["./lib/stats/callstatsio.js", "./lib/index.ts"],
-    devtool:
-      env.development || env.nominify ? "eval" : env.npm ? "none" : false,
+    mode:  env.development || env.nominify || env.npm ? 'development' : 'production',
+    entry: env.npm ? [
+      './lib/index.ts',
+    ] : [
+      './lib/stats/callstatsio.js',
+      './lib/index.ts',
+    ],
+    devtool: env.development || env.nominify ? 'eval' : env.npm ? 'none' : false,
     node: {
       console: false,
-      fs: "empty",
-      net: "empty",
-      tls: "empty",
-      child_process: "empty",
-      module: "empty",
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
     },
     module: {
-      rules: [
-        !env.npm
-          ? {
-              test: path.resolve(__dirname, "./lib/stats/callstatsio.js"),
-              use: ["script-loader"],
-            }
-          : {},
-        {
+      rules: [!env.npm ? {
+          test: path.resolve(__dirname, './lib/stats/callstatsio.js'),
+          use: [ 'script-loader' ]
+        } : {}, {
           test: /\.ts$/,
-          loader: "string-replace-loader",
+          loader: 'string-replace-loader',
           options: {
             multiple: [
               // { search: 'require.*debug.*JsSIP', replace: "require('debug')('JsSIP", flags: 'g' },
@@ -95,10 +89,6 @@ module.exports = (env) => {
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
         { test: /\.js$/, loader: "source-map-loader" },
         {
-          test: /.node$/,
-          loader: "node-loader",
-        },
-        {
           test: /\.mp3$/,
           loader: 'file-loader',
           options: {
@@ -107,54 +97,48 @@ module.exports = (env) => {
         }
       ],
     },
-    output: env.npm
-      ? {
-          filename: "plivobrowsersdk.js",
-          path: path.resolve(__dirname, "dist"),
-          library: "Plivo",
-          libraryTarget: "umd",
-        }
-      : {
-          filename:
-            env.production && !env.nominify
-              ? "plivobrowsersdk.min.js"
-              : "plivobrowsersdk.js",
-          path: path.resolve(__dirname, "dist"),
-          library: "Plivo",
-        },
+    output: env.npm ? {
+      filename: 'plivobrowsersdk.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'Plivo', 
+      libraryTarget: 'umd'
+    } : {
+      filename: env.production && !env.nominify ? 'plivobrowsersdk.min.js' : 'plivobrowsersdk.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'Plivo',
+    },
     watch: !env.production,
     plugins: plugins,
-    devServer: env.development
-      ? {
-          contentBase: path.join(__dirname, "dist"),
-          compress: true,
-          port: 9000,
-        }
-      : {},
+    devServer: env.development ? {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    } : {},
     optimization: {
       minimize: true,
-      minimizer: [
-        new TerserPlugin({
+      minimizer: [new TerserPlugin(
+        {
           terserOptions: {
-            compress: {
-              ecma: 5,
-              warnings: false,
-              comparisons: false,
-            },
+            compress: { 
+              ecma: 5, 
+              warnings: false, 
+              comparisons: false 
+            }, 
           },
-        }),
-      ],
+        }
+      )],
     },
-    resolve: {
-      extensions: [".ts", ".js", ".json"],
-    },
+    resolve:{
+      extensions: ['.ts', '.js','.json']
+    }
   };
 };
 
-function DtsBundlePlugin() {}
+
+function DtsBundlePlugin(){}
 DtsBundlePlugin.prototype.apply = function (compiler) {
-  compiler.plugin("done", function () {
-    var dts = require("dts-bundle");
+  compiler.plugin('done', function(){
+    var dts = require('dts-bundle');
 
     dts.bundle({
       name: 'plivo-browser-sdk',
