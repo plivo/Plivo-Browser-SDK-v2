7c2723cd8da32eee7e5a77970b5670c5f1f5a2cd
Karthik Dechiraju <karthik.dechiraju@plivo.com>
2021-03-09 12:38:41 +0530

    add junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,17 @@ module.exports = function (config) {
             transform: [ ["envify", env], [["babelify", { extensions: [ '.ts', '.js' ]}]] ],
             plugin: [ ["tsify",{target: 'es6'}] ]
         },
-        reporters: ['progress', 'html'],
+        reporters: ['progress', 'mocha', 'html', 'junit'],
+        junitReporter: {
+            outputDir: 'report', // results will be saved as $outputDir/$browserName.xml
+            outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+            suite: '', // suite will become the package name attribute in xml testsuite element
+            useBrowserName: true, // add browser name to report and classes names
+            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+            classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+            properties: {}, // key value pair of properties to add to the <properties> section of the report
+            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+        },
         htmlReporter: {
             outputDir: 'report', // where to put the reports 
             templatePath: null, // set if you moved jasmine_template.html
