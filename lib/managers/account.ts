fe123faded452810ea5ecc99417eb63b0aa8d62a
Karthik Dechiraju <karthik.dechiraju@plivo.com>
2021-06-14 15:28:29 +0530

    Fix firefox mediasetup time issue

--- lib/managers/account.ts
+++ lib/managers/account.ts
@@ -407,8 +407,8 @@ class Account {
     } else {
       createOutgoingSession(evt);
     }
-    this.cs.timeTakenForStats.iceConnection = {} as any;
-    this.cs.timeTakenForStats.iceConnection.init = new Date().getTime();
+    this.cs.timeTakenForStats.mediaSetup = {} as any;
+    this.cs.timeTakenForStats.mediaSetup.init = new Date().getTime();
   };
 
   /**
